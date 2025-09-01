85cd7d8bcd66f72bfd86b06b3fff865edef69e6d
Steviamp <74257777+Steviamp@users.noreply.github.com>
2025-09-01 10:27:21 +0300

    Add files via upload

---
+++ QWinpanel-main/QWinpanel/Services/Requests/CallTicketRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QWinpanel.Services.Requests
+{
+    public class CallTicketRequest
+    {
+        [Required]
+        public string cashierNr { get; set; }
+
+        [Required]
+        public string host { get; set; }
+
+        [Required]
+        public int ticketTrack { get; set; }
+    }
+}
